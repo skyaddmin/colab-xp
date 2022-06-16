02bde7e731939a55ee1ec6d0757e5914c81ec74a
skyaddmin <85551616+skyaddmin@users.noreply.github.com>
2022-06-16 19:08:04 +0700

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -2,7 +2,7 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/nu
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null &
+nohup ./ngrok tcp --region jp 5900 &>/dev/null &
 echo Please wait for installing...
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
